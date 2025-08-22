706aaafdf8275ef318386726fbf741498a62092a
Jasmetk0 <50439468+Jasmetk0@users.noreply.github.com>
2025-08-22 15:42:10 +0200

    feat: browser-like shell homepage UI

---
+++ shell/tests.py
@@ -0,0 +1,12 @@
+from django.urls import reverse
+
+
+def test_home_page(client):
+    url = reverse("home")
+    response = client.get(url)
+    assert response.status_code == 200
+
+
+def test_homepage_contains_wiki(client):
+    response = client.get("/")
+    assert b"Wiki" in response.content
