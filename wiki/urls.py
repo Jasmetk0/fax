a7de5fcfc8d414568a9d273b438d36a0de935f35
Jasmetk0 <50439468+Jasmetk0@users.noreply.github.com>
2025-08-22 13:10:05 +0200

    style: format wiki models

---
+++ wiki/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = "wiki"
+
+urlpatterns = [
+    path("", views.ArticleListView.as_view(), name="article-list"),
+    path("create/", views.ArticleCreateView.as_view(), name="article-create"),
+    path("<slug:slug>/edit/", views.ArticleUpdateView.as_view(), name="article-edit"),
+    path("<slug:slug>/", views.ArticleDetailView.as_view(), name="article-detail"),
+]
