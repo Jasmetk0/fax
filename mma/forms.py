712df6b1b06dd2cd67a8cd32cfca1bf8e467aa9f
Jasmetk0 <50439468+Jasmetk0@users.noreply.github.com>
2025-08-26 11:48:06 +0200

    feat: add custom mma admin pages

---
+++ mma/forms.py
@@ -0,0 +1,86 @@
+from django import forms
+
+from .models import (
+    Bout,
+    Event,
+    Fighter,
+    Organization,
+    Ranking,
+    NewsItem,
+)
+
+
+class OrganizationForm(forms.ModelForm):
+    class Meta:
+        model = Organization
+        fields = ["slug", "name", "short_name"]
+
+
+class EventForm(forms.ModelForm):
+    class Meta:
+        model = Event
+        fields = [
+            "slug",
+            "organization",
+            "name",
+            "date_start",
+            "venue",
+            "is_completed",
+        ]
+
+
+class FighterForm(forms.ModelForm):
+    class Meta:
+        model = Fighter
+        fields = [
+            "slug",
+            "first_name",
+            "last_name",
+            "nickname",
+            "country",
+            "stance",
+            "height_cm",
+            "reach_cm",
+            "birthdate",
+        ]
+
+
+class BoutForm(forms.ModelForm):
+    class Meta:
+        model = Bout
+        fields = [
+            "event",
+            "weight_class",
+            "fighter_red",
+            "fighter_blue",
+            "result",
+            "method",
+            "round",
+            "time",
+            "is_title_fight",
+        ]
+
+
+class RankingForm(forms.ModelForm):
+    class Meta:
+        model = Ranking
+        fields = [
+            "organization",
+            "weight_class",
+            "position",
+            "fighter",
+            "date_effective",
+        ]
+
+
+class NewsItemForm(forms.ModelForm):
+    class Meta:
+        model = NewsItem
+        fields = [
+            "slug",
+            "title",
+            "summary",
+            "content",
+            "source_url",
+            "published_at",
+        ]
