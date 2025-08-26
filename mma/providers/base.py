3ed893a42ac19031538a81511913bb162d6ae004
Jasmetk0 <50439468+Jasmetk0@users.noreply.github.com>
2025-08-26 09:56:49 +0200

    Add MMA event and news API endpoints

---
+++ mma/providers/base.py
@@ -0,0 +1,28 @@
+"""Base provider interface for MMA data."""
+
+from abc import ABC, abstractmethod
+from typing import Iterable, Mapping
+
+
+class MMADataProvider(ABC):
+    """Abstract base class for MMA data providers."""
+
+    @abstractmethod
+    def events(self) -> Iterable[Mapping]:
+        """Return iterable of event data."""
+
+    @abstractmethod
+    def results(self) -> Iterable[Mapping]:
+        """Return iterable of result data."""
+
+    @abstractmethod
+    def fighters(self) -> Iterable[Mapping]:
+        """Return iterable of fighter data."""
+
+    @abstractmethod
+    def rankings(self) -> Iterable[Mapping]:
+        """Return iterable of ranking data."""
+
+    @abstractmethod
+    def news(self) -> Iterable[Mapping]:
+        """Return iterable of news items."""
